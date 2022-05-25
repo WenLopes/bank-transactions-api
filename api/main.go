a78036ca7aefff59e726978ebec7deec9b36ed8b
Wendel Lopes <wendel.lopes@picpay.com>
2022-05-25 20:10:48 -0300

    adicionando handlers ao main

--- api/main.go
+++ api/main.go
@@ -5,11 +5,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/WenLopes/bank-transactions-api/api/handlers"
+	"github.com/WenLopes/bank-transactions-api/app/account"
+	"github.com/WenLopes/bank-transactions-api/infra/repository"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	accountRepository := repository.NewAccountInStateRepository()
+	accountService := account.NewService(accountRepository)
+
 	router := mux.NewRouter()
+	handlers.NewEventHandler(router, accountService)
+	handlers.NewBalanceHandler(router, accountService)
+
 	fmt.Printf("Escutando na porta LOCAL 16091 🏆 \n")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
