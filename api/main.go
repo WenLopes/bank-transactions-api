6161f69d7ac8c7479ff7dcae6b07ae48169970dd
Wendel Lopes <wendel.lopes@picpay.com>
2022-05-25 20:03:54 -0300

    removendo hello world

--- api/main.go
+++ api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,17 +10,6 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", helloWorld).Methods("GET")
 	fmt.Printf("Escutando na porta LOCAL 16091 🏆 \n")
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-
-	helloWorld := new(struct {
-		Message string `json:"message"`
-	})
-	helloWorld.Message = "Hello world!"
-
-	json.NewEncoder(w).Encode(helloWorld)
 }
