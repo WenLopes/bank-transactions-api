c7e3335caa167dd06e51b98768e6d6c591bfecf0
Wendel Lopes <wendel.lopes@picpay.com>
2022-05-25 20:29:18 -0300

    criando direcionamento de eventos para seus respectivos handlers

--- api/handlers/event.go
+++ api/handlers/event.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/WenLopes/bank-transactions-api/api/requests"
+	"github.com/WenLopes/bank-transactions-api/api/responses"
 	"github.com/WenLopes/bank-transactions-api/app/account"
 	"github.com/gorilla/mux"
 )
+
+var eventHandleable = map[string]func(w http.ResponseWriter, event requests.EventRequest, accountService account.UseCase){
+	"deposit":  handleDeposit,
+	"transfer": handleTransfer,
+	"withdraw": handleWithDraw,
+}
 
 func NewEventHandler(router *mux.Router, accountService account.UseCase) {
 	router.HandleFunc("/event", newEvent(accountService)).Methods("POST").Name("newEvent")
@@ -14,6 +22,36 @@ func NewEventHandler(router *mux.Router, accountService account.UseCase) {
 
 func newEvent(accountService account.UseCase) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("todo: executar evento")
+		event, err := requests.MapRequestToEvent(request)
+		if err != nil {
+			responses.Error(writer, http.StatusUnprocessableEntity, err) // NÃO DEVOLVER MENSAGEM DE ERRO PARA O USUÁRIO
+			return
+		}
+
+		eventHandleable[event.EventType](writer, event, accountService)
 	}
+}
+
+func handleDeposit(
+	writer http.ResponseWriter,
+	event requests.EventRequest,
+	accountService account.UseCase,
+) {
+	fmt.Println("handle deposit")
+}
+
+func handleWithDraw(
+	writer http.ResponseWriter,
+	event requests.EventRequest,
+	accountService account.UseCase,
+) {
+	fmt.Println("handle withdraw")
+}
+
+func handleTransfer(
+	writer http.ResponseWriter,
+	event requests.EventRequest,
+	accountService account.UseCase,
+) {
+	fmt.Println("handle transfer")
 }
