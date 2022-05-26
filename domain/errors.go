7c28a4e3fade001b559044b92d8d4946c2b59aec
Wendel Lopes <wendel.lopes@picpay.com>
2022-05-26 12:05:47 -0300

    adicionando erros de dominio

---
+++ domain/errors.go
@@ -0,0 +1,7 @@
+package domain
+
+const (
+	ErrInvalidAmount          = "valor para operação inválido"
+	BlockedAccount            = "conta bloqueada temporariamente para transacionar"
+	InsuficientAccountBalance = "não há saldo em carteira para realização da operação"
+)
