dfd7b25a609ae0e99604d44a8f072a70c3aa143d
Wendel Lopes <wendel.lopes@picpay.com>
2022-05-26 12:12:13 -0300

    adicionando validadores de transfer e withdraw

--- api/validators/validator.go
+++ api/validators/validator.go
@@ -3,18 +3,14 @@ package validators
 import (
 	"errors"
 
+	"github.com/WenLopes/bank-transactions-api/api/messages"
 	"github.com/WenLopes/bank-transactions-api/api/requests"
 )
 
 var validEventTypes = [3]string{"deposit", "transfer", "withdraw"}
 
 func ValidateDeposit(event requests.EventRequest) (bool, error) {
-	valid, err := eventTypeIsValid(event.EventType)
-	if !valid {
-		return false, err
-	}
-
-	valid, err = amountIsValid(event.Amount)
+	valid, err := validateCommonEvent(event)
 	if !valid {
 		return false, err
 	}
@@ -27,10 +23,58 @@ func ValidateDeposit(event requests.EventRequest) (bool, error) {
 	return true, nil
 }
 
+func ValidateTransfer(event requests.EventRequest) (bool, error) {
+	valid, err := validateCommonEvent(event)
+	if !valid {
+		return false, err
+	}
+
+	valid, err = originIsValid(event.Origin)
+	if !valid {
+		return false, err
+	}
+
+	valid, err = destinationIsValid(event.Destination)
+	if !valid {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func ValidateWithDraw(event requests.EventRequest) (bool, error) {
+	valid, err := validateCommonEvent(event)
+	if !valid {
+		return false, err
+	}
+
+	valid, err = originIsValid(event.Origin)
+	if !valid {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func validateCommonEvent(event requests.EventRequest) (bool, error) {
+
+	valid, err := eventTypeIsValid(event.EventType)
+	if !valid {
+		return false, err
+	}
+
+	valid, err = amountIsValid(event.Amount)
+	if !valid {
+		return false, err
+	}
+
+	return true, nil
+}
+
 func eventTypeIsValid(eventType string) (bool, error) {
 
 	if eventType == "" {
-		return false, errors.New("o tipo do evento não é válido")
+		return false, errors.New(messages.INVALID_EVENT_TYPE)
 	}
 
 	for _, validEventType := range validEventTypes {
@@ -39,13 +83,13 @@ func eventTypeIsValid(eventType string) (bool, error) {
 		}
 	}
 
-	return false, errors.New("o tipo do evento não é válido")
+	return false, errors.New(messages.INVALID_EVENT_TYPE)
 }
 
 func amountIsValid(amount float32) (bool, error) {
 
 	if amount < 0 {
-		return false, errors.New("o valor informado não é válido")
+		return false, errors.New(messages.INVALID_AMOUNT_VALUE)
 	}
 
 	return true, nil
@@ -53,7 +97,15 @@ func amountIsValid(amount float32) (bool, error) {
 
 func destinationIsValid(destination string) (bool, error) {
 	if destination == "" {
-		return false, errors.New("o campo destination é obrigatório para realizar essa operação")
+		return false, errors.New(messages.DESTINATION_FIELD_REQUIRED)
+	}
+
+	return true, nil
+}
+
+func originIsValid(origin string) (bool, error) {
+	if origin == "" {
+		return false, errors.New(messages.ORIGIN_FIELD_REQUIRED)
 	}
 
 	return true, nil
